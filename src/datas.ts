1f99170a7da60ac6bb3f2fd02ef2af10bb14f235
snrtn <snrtn94@gmail.com>
2021-12-30 16:06:38 +0100

    add datas

--- src/datas.ts
+++ src/datas.ts
@@ -23,7 +23,7 @@ export const sliderItems = [
   {
     id: 4,
     img: 'https://images.pexels.com/photos/4578665/pexels-photo-4578665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    title: 'Web Component',
+    title: 'Web Framework ',
     desc: "Les derniers cours que j'ai suivis en autodidacte sont sur du React.",
     bg: '4169e111',
   },
@@ -34,12 +34,64 @@ export const faqItems = [
     id: 1,
     title: 'Comment puis-je vois le code de source ?',
     desc: 'Vous pouvez trouver le source code ici : ',
-    adress: 'https://github.com/snrtn/presentation',
+    adress: 'https://github.com/snrtn/typereact',
   },
   {
     id: 2,
     title: 'Puis-je savoir plus sur vous ?',
     desc: "Oui, n'hésitez pas à me contacter. Vous pouvez aussi consulter mon test personnalité ici : ",
     adress: 'https://app.assessfirst.com/_/profile/7somo4wm-hanjun-kim',
+  },
+];
+
+export const profilitems = [
+  {
+    img: 'https://images.pexels.com/photos/4114585/pexels-photo-4114585.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  {
+    img: 'https://images.pexels.com/photos/8427638/pexels-photo-8427638.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  },
+];
+
+export const experienceitems = [
+  {
+    title: 'Machine-outil équipe',
+    subtitle:
+      'Séoul (Corée du Sud / Dongdaemun-gu) – formation au sein de l’école',
+    data: 'Période : 2010-2012',
+    dect: 'Maintenance de la machine',
+    decta: 'Fabrication de pièces automobile/moto',
+    dectb: '',
+    img: 'https://images.pexels.com/photos/48799/drill-milling-milling-machine-drilling-48799.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  {
+    title: 'Armée de l’air',
+    subtitle:
+      '***** (Corée du Sud / **th Fighter Wing) – Service militaire obligatoire en Corée du Sud Mission : Sergent',
+    data: 'Période : 2013-2015',
+    dect: 'En charge de la maintenance technique des avions de chasse',
+    decta:
+      'En charge de l’inventaire, du nettoyage et de la distribution des armes',
+    dectb: '',
+    img: 'https://images.pexels.com/photos/733254/pexels-photo-733254.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  {
+    title: 'POSCO',
+    subtitle:
+      'Séoul (Corée du Sud / Gannam-gu) – Entreprise dans la production d’acier',
+    data: 'Période : 2015-2018',
+    dect: 'En charge de la protection du PDG',
+    decta: '',
+    dectb: '',
+    img: 'https://www.ild-security.com/wp-content/uploads/2018/01/20.jpg',
+  },
+  {
+    title: 'E.Leclerc',
+    subtitle: 'Montévrain – Grande surface',
+    data: 'Période : 2019-2020',
+    dect: 'Nettoyage des rayons',
+    decta: 'Mettre les aliments et produits en rayon',
+    dectb: 'Organisation du magasin',
+    img: 'https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   },
 ];
