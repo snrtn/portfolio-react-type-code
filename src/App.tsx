f980f3b35aa9c52fe7e8ba8cc21dc73646273d4b
snrtn <snrtn94@gmail.com>
2021-12-30 16:06:49 +0100

    change variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import React from 'react';
 import Navbar from './Components/Navbar';
-
 import Home from 'Pages/Home';
+import Curriculum from 'Pages/Curriculum';
+import Feedback from 'Pages/Feedback';
+import Footer from 'Components/Footer';
 
-import { Container } from './app.styles';
+import { Container } from './Styles/app.styles';
+
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Container>
-      <Navbar />
-      <Home />
-    </Container>
+    <Router>
+      <Container>
+        <Navbar />
+        <Routes>
+          <Route caseSensitive={false} path="/" element={<Home />} />
+          <Route
+            caseSensitive={false}
+            path="/curriculum"
+            element={<Curriculum />}
+          />
+          <Route
+            caseSensitive={false}
+            path="/feedback"
+            element={<Feedback />}
+          />
+        </Routes>
+        <Footer />
+      </Container>
+    </Router>
   );
 };
 
---
+++ src/Styles/app.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import variables from '../Variables/variables';
+
+export const Container = styled.main`
+  width: 100%;
+  height: 100%;
+  background-color: ${variables.white};
+  overflow: hidden;
+`;
